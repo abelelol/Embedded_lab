6732a3a1250b18129f593e9864869c1ca127966e
abelelol <abeleamsalu@gmail.com>
2019-04-04 21:53:10 -0400

    Creation of project

---
+++ src/Transfer.java
@@ -0,0 +1,9 @@
+public class Transfer {
+
+    public static void main (String args[]){
+
+
+
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+
+
+public class User {
+    private String p;
+    private String n;
+
+    public User(){
+        this.p="";
+        this.n="";
+
+    }
+    public User(String n, String p){
+        this.n=n;
+        this.p=p;
+    }
+
+}
